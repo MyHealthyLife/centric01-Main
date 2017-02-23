100c03495dfb18ec89db803b7cc9fe6169262646
StefanoTavonatti93 <stefano.tavonatti@studenti.unitn.it>
2017-02-23 10:00:36 +0100

    Get list of people by service01

---
+++ centric01-main/src/myhealthylife/centric1/rest/PeopleHandler.java
@@ -0,0 +1,30 @@
+package myhealthylife.centric1.rest;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import myhealthylife.centric1.util.ServicesLocator;
+import myhealthylife.centric1.util.Utilities;
+import myhealthylife.dataservice.soap.DataService;
+import myhealthylife.dataservice.soap.People;
+
+@Path("/people")
+public class PeopleHandler {
+
+	@GET
+    @Produces({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
+    @Consumes({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
+	public Response getPeople(){
+		DataService ds=ServicesLocator.getDataServiceConnection();
+		People people=ds.listPeople();
+		
+		if(people==null)
+			return Utilities.throwResourceNotFound();
+		
+		return Utilities.throwOK(people);
+	}
+}
