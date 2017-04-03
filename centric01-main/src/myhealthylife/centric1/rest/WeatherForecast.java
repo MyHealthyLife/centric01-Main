7fda9268a1fb5a6aca4124df8d54c1af9038fcd6
StefanoTavonatti93 <stefano.tavonatti@studenti.unitn.it>
2017-04-03 14:15:24 +0200

    weather forecast and wsimport update

---
+++ centric01-main/src/myhealthylife/centric1/rest/WeatherForecast.java
@@ -0,0 +1,34 @@
+package myhealthylife.centric1.rest;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import myhealthylife.centric1.util.ServicesLocator;
+import myhealthylife.dataservice.soap.Current;
+import myhealthylife.dataservice.soap.DataService;
+import myhealthylife.dataservice.soap.Person;
+
+@Path("/weather")
+public class WeatherForecast {
+
+	@GET
+	@Path("/{username}")
+	@Produces({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
+    @Consumes({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
+	public Current getWeatherForecast(@PathParam("username") String username){
+		DataService ds=ServicesLocator.getDataServiceConnection();
+		
+		Person p=ds.getPersonByUsername(username);
+		
+		if(p==null)
+			return null;
+		
+		Current weather=ds.getWeatherForecast(p.getIdPerson());
+		
+		return weather;
+	}
+}
