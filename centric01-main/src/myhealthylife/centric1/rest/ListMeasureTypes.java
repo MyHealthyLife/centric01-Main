1a15db3ca67b5b46b421535ef0ed6787b7df31af
StefanoTavonatti93 <stefano.tavonatti@studenti.unitn.it>
2017-04-27 14:16:17 +0200

    list measure types documentation

--- centric01-main/src/myhealthylife/centric1/rest/ListMeasureTypes.java
+++ centric01-main/src/myhealthylife/centric1/rest/ListMeasureTypes.java
@@ -12,6 +12,10 @@ import myhealthylife.dataservice.soap.MeasureTypes;
 @Path("/measuretypes")
 public class ListMeasureTypes {
 
+	/**
+	 * The result of this request contains a list of measureType object which contains only a string with the name of the category
+	 * @return
+	 */
 	@GET
 	@Produces({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
     @Consumes({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
